d4140ac1b541d4a6db656b455558fe3f7938ac33
Weldybox <43091716+Weldybox@users.noreply.github.com>
2019-05-12 20:34:38 +0200

    Favoris
    ajout partiel couleur préféré

--- ESP8266_LED_ColorPick/data/js/script.js
+++ ESP8266_LED_ColorPick/data/js/script.js
@@ -10,11 +10,71 @@ function init() {
   Socket = new WebSocket('ws://' + window.location.hostname + ':82/');
 }
 
+window.onload=function(){
+
+  var bouton = document.getElementById('btnMenu');
+  var nav = document.getElementById('nav');
+  bouton.onclick = function(e){
+      if(nav.style.display=="block"){
+          nav.style.display="none";
+      }else{
+          nav.style.display="block";
+      }
+  };
+};
+
+var position = [];
+function chooseSave(){
+  Papa.parse('save.csv', {
+    header: false,
+    download: true,
+    dynamicTyping: true,
+    complete: function(results) {
+      console.log(results);
+      for(var i=(((results.data[1]).length)-2);i>=0;i-=1){
+        console.log(((results.data[1]).length));
+        if(i > (((results.data[1]).length)-4)){
+          var id= 'carre' + String((i%4)+1);
+          var backgroundcolor = "rgb(" + (results.data[1])[i] + ")";
+          position[parseInt((id.substr(5,6)),10)] = i;
+          document.getElementById(id).style.background = backgroundcolor;
+        }
+      }
+    }
+  });
+}
+
+function setSaveColor(id){
+  Papa.parse('save.csv', {
+    header: false,
+    download: true,
+    dynamicTyping: true,
+    complete: function(results) {
+      console.log(position);
+      var tableau = results.data[1];
+      console.log(tableau[parseInt((id.substr(5,6)),10)-1]);
+      colorPicker.color.rgbString = "rgb("+ tableau[position[parseInt((id.substr(5,6)),10)]] +")";
+      console.log(index);
+    }
+  });
+}
+
+function saving(){
+
+  Socket.send("sR"+red);
+  Socket.send("sG"+green);
+  Socket.send("sB"+blue);
+
+}
 setInterval(function() {
   if(on){
+    /*var sauv = document.getElementById("sauv");
+    sauv.style.backgroundColor = "rgb("+ red + "," + green + "," + blue + ")";*/
+
     Socket.send("R"+red);
     Socket.send("G"+green);
     Socket.send("B"+blue);
+
   }
 }, 500);
 
@@ -37,17 +97,17 @@ var colorPicker = new iro.ColorPicker(".colorPicker", {
   // color picker options
   // Option guide: https://iro.js.org/guide.html#color-picker-options
   width: 350,
-  color: "rgb(255, 0, 0)",
+  //color: "rgb(255, 0, 0)",
   borderWidth: 1,
   borderColor: "#fff",
 });
 
  colorPicker.on(["color:init", "color:change"], function(color){
+
     red = ((color.rgbString).split(',')[0]).substring(4, 7);
     green = (color.rgbString).split(',')[1];
         // if((color.rgbString).split(',')[2]) ==
     var longueur =  ((color.rgbString).split(',')[2]).length;
     blue = (((color.rgbString).split(',')[2]).substring(0,longueur-1));
-
 
 });
--- ESP8266_LED_ColorPick/src/main.cpp
+++ ESP8266_LED_ColorPick/src/main.cpp
@@ -57,33 +57,85 @@ void confOTA() {
   ArduinoOTA.begin();
 }
 
+
+
+bool checkSpace(){
+  File file = SPIFFS.open("/save.csv", "r");
+ 
+  Serial.println("File Content:");
+  uint8_t count = 0;
+  String save[] = {};
+  /*while(file.available()){
+    String part = file.readStringUntil(';');
+    Serial.println(part);
+    count++;
+    save[count] = part;
+    if(count >= 4){
+      Serial.println(save[0]);
+    }
+  }*/
+  file.close();
+
+  return 1;
+}
+/*--------------------------------------------------------------------------------
+Fonction qui traite les requêtes websocket arrivant depuis le serveur web.
+--------------------------------------------------------------------------------*/
+void addData(uint16_t couleur, uint8_t * couleurSave){
+  uint16_t save = (uint16_t) strtol((const char *) &couleurSave[2], NULL, 10);
+  File f = SPIFFS.open("/save.csv", "a+");
+  if (!f) {
+    Serial.println("erreur ouverture fichier!");
+  }else if(checkSpace()){
+    if(couleur == 'R'){
+        char buffred[16];
+        sprintf(buffred,"%d,",save);
+        Serial.println(buffred);
+        f.print(buffred);
+    }if(couleur == 'G'){
+        char buffgreen[16];
+        sprintf(buffgreen,"%d,",save);
+        Serial.println(buffgreen);
+        f.print(buffgreen);
+        
+    }if(couleur == 'B'){
+        char buffblue[16];
+        sprintf(buffblue,"%d;",save);
+        Serial.println(buffblue);
+        f.print(buffblue);
+    }
+    f.close();
+  }
+}
 /*--------------------------------------------------------------------------------
 Fonction qui traite les requêtes websocket arrivant depuis le serveur web.
 --------------------------------------------------------------------------------*/
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length){
   if(type == WStype_TEXT){
+    uint16_t couleur = (uint16_t) strtol((const char *) &payload[1], NULL, 10);
+    if(payload[0] == 'R'){
+      analogWrite(REDPIN, couleur);
+      delay(FADESPEED);
+
+      Serial.print("rouge = ");
+      Serial.println(couleur);
+    }
     if(payload[0] == 'G'){
-      uint16_t vertLevel = (uint16_t) strtol((const char *) &payload[1], NULL, 10);
-      Serial.print("niveauVert = ");
-      Serial.println(vertLevel);
-
-      analogWrite(REDPIN, vertLevel);
+      analogWrite(GREENPIN, couleur);
       delay(FADESPEED);
 
-    }else if(payload[0] == 'R'){
-      uint16_t redLevel = (uint16_t) strtol((const char *) &payload[1], NULL, 10);
-      Serial.print("niveauRouge = ");
-      Serial.println(redLevel);
-
-      analogWrite(GREENPIN, redLevel);
+      Serial.print("vert = ");
+      Serial.println(couleur);
+    }
+    if(payload[0] == 'B'){
+      analogWrite(BLUEPIN, couleur);
       delay(FADESPEED);
-    }else if(payload[0] == 'B'){
-      uint16_t blueLevel = (uint16_t) strtol((const char *) &payload[1], NULL, 10);
-      Serial.print("niveauBleu = ");
-      Serial.println(blueLevel);
 
-      analogWrite(BLUEPIN, blueLevel);
-      delay(FADESPEED);
+      Serial.print("bleu = ");
+      Serial.println(couleur);
+    }
+    if(payload[0] =='s'){
+      addData(payload[1],payload);
     }
   }
 
@@ -123,6 +175,7 @@ void setup() {
   server.serveStatic("/", SPIFFS, "/index.html");
   server.serveStatic("/js", SPIFFS, "/js");
   server.serveStatic("/style", SPIFFS, "/style");
+  server.serveStatic("/save.csv", SPIFFS, "/save.csv");
 
   server.begin();
   webSocket.begin();
